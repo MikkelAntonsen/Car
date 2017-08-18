a37fff63ee46443a98fea65f8e6463e8a0752a28
Mikkel Antonsen <mikkelntnsn@gmail.com>
2017-08-18 21:21:54 +0000

    Program shouldn't crash on trash input (which tends to happen right after startup).

--- raspberry_pi/src/ArduinoInterface.cpp
+++ raspberry_pi/src/ArduinoInterface.cpp
@@ -4,40 +4,10 @@ void parse_arduino_input(std::string input, std::vector<int> &result)
 {
     std::size_t start = input.find(':', 0) + 1;
     std::size_t end = input.find(' ', 0) - 1;
-
-    if (std::string::npos == start || std::string::npos == end) {
-        result[0]=-1;
-        result[1]=-1;
-        return;
-    }
-
-    for (size_t i=start; i<end; i++) {
-        if (!isdigit(input[i])) {
-            result[0]=-1;
-            result[1]=-1;
-            return;
-        }
-    }
-    std::cout<<std::endl;
-
     int engine_value = stoi(input.substr(start, end));
     result[0] = engine_value;
 
     start = input.find(':', end) + 1;
-    if (std::string::npos == start) {
-        result[0]=-1;
-        result[1]=-1;
-        return;
-    }
-
-    for (size_t i=start; start<end; i++) {
-        if (!isdigit(input[i])) {
-            result[0]=-1;
-            result[1]=-1;
-            return;
-        }
-    }
-
     int steer_value = stoi(input.substr(start));
     result[1] = steer_value;
 }
