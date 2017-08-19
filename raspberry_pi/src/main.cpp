b4075856abc9eb95f13b22801f17743d70cd21ab
Mikkel Antonsen <mikkelntnsn@gmail.com>
2017-08-19 12:48:49 +0200

    Test how long it takes to take a picture.

--- raspberry_pi/src/main.cpp
+++ raspberry_pi/src/main.cpp
@@ -2,16 +2,22 @@
 #include <fstream>
 #include <unistd.h>
 #include <cstring>
-#include "serial-lib.h"
-#include "ArduinoInterface.h"
 #include <vector>
 #include <string>
+#include <time.h>
 #include <raspicam/raspicam.h>
+
+#include "serial-lib.h"
+#include "ArduinoInterface.h"
+
+#define LOG_FILENAME "episode.log"
+#define CONTROL_INPUT_FILENAME "episode_control.data"
+#define IMAGES_FILENAME "episode_images.data"
 
 int main()
 {
     std::ofstream log_file;
-    log_file.open("episode_1.log");
+    log_file.open(LOG_FILENAME);
 
     raspicam::RaspiCam Camera;
     if ( !Camera.open() ) {
@@ -22,45 +28,49 @@ int main()
     /*
      *  Initialize serial communiations
      */
-    int fd = -1;
-    while (-1 == fd) {
-        fd = serialport_init("/dev/ttyACM0", 4800);
-        sleep(1);
-        log_file << "Unable to open /dev/ttyACM0 for reading: " << fd << std::endl;
-    }
+    // int fd = -1;
+    // while (-1 == fd) {
+    //     fd = serialport_init("/dev/ttyACM0", 4800);
+    //     sleep(1);
+    //     log_file << "Unable to open /dev/ttyACM0 for reading: " << fd << std::endl;
+    // }
 
     /*
      *  Initialize  output files
      */
-    std::ofstream image_file("raspicam_test.ppm", std::ios::binary);
+    std::ofstream image_file(IMAGES_FILENAME, std::ios::binary);
     unsigned char image[Camera.getImageTypeSize(raspicam::RASPICAM_FORMAT_RGB)];
 
-    Camera.grab();
-    Camera.retrieve(image, raspicam::RASPICAM_FORMAT_IGNORE);
-    image_file << "P6" << std::endl << Camera.getWidth() << " " << Camera.getHeight() << " 255" << std::endl;
-    image_file.write((char *) image, Camera.getImageTypeSize(raspicam::RASPICAM_FORMAT_RGB));
-    image_file.close();
-
     std::ofstream output_file;
-    output_file.open("episode_1.data");
+    output_file.open(CONTROL_INPUT_FILENAME);
 
-    std::vector<int> control_values(2, 0);
-    char buf[100];
+    // std::vector<int> control_values(2, 0);
+    // char buf[100];
     std::cout << "Starting..." << std::endl;
     while (1) {
 
-        serialport_readline(fd, buf, 100, 100);
+        int t0 = time(NULL);
+        Camera.grab();
+        Camera.retrieve(image, raspicam::RASPICAM_FORMAT_IGNORE);
+        image_file << "P6" << std::endl << Camera.getWidth() << " " << Camera.getHeight() << " 255" << std::endl;
+        image_file.write((char *) image, Camera.getImageTypeSize(raspicam::RASPICAM_FORMAT_RGB));
+        image_file << std::endl;
+        std::cout << "Time: " << time(NULL) - t0 << "s" << std::endl;
 
-        for (int i=0; buf[i]!=0; ++i) {
-            std::cout << buf[i];
-        }
-        std::cout << std::endl;
+
+        // serialport_readline(fd, buf, 100, 100);
+        // for (int i=0; buf[i]!=0; ++i) {
+        //     std::cout << buf[i];
+        // }
+        // std::cout << std::endl;
 
 
-        parse_arduino_input(std::string(buf, 100), control_values);
-        std::cout << "engine:"<<control_values[0] << " steer:" << control_values[1] << std::endl;
-        output_file << control_values[0] << " " << control_values[1] << std::endl;
+        // parse_arduino_input(std::string(buf, 100), control_values);
+        // std::cout << "engine:"<<control_values[0] << " steer:" << control_values[1] << std::endl;
+        // output_file << control_values[0] << " " << control_values[1] << std::endl;
     }
     output_file.close();
+    image_file.close();
+    log_file.close();
     return 0;
 }
